8bf0e82848f9640de7bcc24d03570f0f85590ca3
CarlosAyala123 <95982346+CarlosAyala123@users.noreply.github.com>
2022-09-20 10:23:41 -0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -24,17 +24,27 @@ library(tidyverse)
 
 #------------------------------------------------------------------------------
 # Cargar las bases de datos
+
+#Mateo
 setwd("~/Programacion/BDML/Predicting-Poverty")
-train_hogares <- readRDS("~/Programacion/BDML/Predicting-Poverty/Stores/data/train_hogares.Rds")
-train_personas <- readRDS("~/Programacion/BDML/Predicting-Poverty/Stores/data/train_personas.Rds")
+hogares <- readRDS("~/Programacion/BDML/Predicting-Poverty/Stores/data/train_hogares.Rds")
+personas <- readRDS("~/Programacion/BDML/Predicting-Poverty/Stores/data/train_personas.Rds")
+
+#Carlos
+setwd("C:/Users/caaya/OneDrive - Universidad de los Andes/universidad/8 semestre/BigData/Problem sets/Problem set 2/Predicting-Poverty")
+hogares <- readRDS("C:/Users/caaya/OneDrive - Universidad de los Andes/universidad/8 semestre/BigData/Problem sets/Problem set 2/Predicting-Poverty/data/train_hogares.Rds")
+personas <- readRDS("C:/Users/caaya/OneDrive - Universidad de los Andes/universidad/8 semestre/BigData/Problem sets/Problem set 2/Predicting-Poverty/data/train_personas.Rds")
+
+#Federico
+
 
 #------------------------------------------------------------------------------
 #  Analísis exploratorio de ambas bases por separado.
-colnames(train_hogares)
-skim(train_hogares)
+colnames(hogares)
+skim(hogares)
 
 
-colnames(train_personas)
+colnames(personas)
   # id presente en ambas tablas
 
 
@@ -44,5 +54,27 @@ colnames(train_personas)
 
 #------------------------------------------------------------------------------
 # Unir las bases
-train <-train_personas %>%  left_join(train_hogares)
+df <-personas %>%  left_join(hogares)
   #Unión de ambas bases.
+
+
+#------------------------------------------------------------------------------
+
+# Generando muestra train, evaluate and test
+
+## 75% of the sample size
+smp_train_size <- floor(0.7 * nrow(df)) ## cambiar el 0.75 por tamaño de muestra train
+
+## set the seed to make your partition reproducible
+set.seed(10101)
+
+train_ind <- sample(seq_len(nrow(df)), size = smp_size)
+
+train <- df[train_ind, ]
+test <- df[-train_ind, ]
+
+smp_eva_size <- floor(0.2 * nrow(df)) ## cambiar el 0.75 por tamaño de muestra eval
+smp_test_size <- floor(0.1 * nrow(df)) ## cambiar el 0.75 por tamaño de muestra train
+
+
+
