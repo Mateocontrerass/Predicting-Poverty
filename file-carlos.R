cd6137288e43e2845b54e1e6f0e9e8c79d9821a8
mateocontreras <95982346+CarlosAyala123@users.noreply.github.com>
2022-09-29 15:35:42 -0500

    Update file-carlos.R

--- file-carlos.R
+++ file-carlos.R
@@ -18,15 +18,13 @@ p_load(tidyverse,dplyr,here,skimr,tidyr,gamlr,modelsummary,caret,
 
 ## Depurar base
 
-set.seed(777) ## fijar semilla divina
+set.seed(666)
 
 training <- data_rf_train
 rm(data_rf_train)
 
 colnames(training)
 
-#sub_set_training <- subset(training, select = -c(id, Li, Lp, P6050, P6210s1, P6426, 
-#                                                P6020, P5010, P5130, Npersug, ing))
 
 sub_set_training <- subset (training, select = -c(id, Li, Lp, ing, Npersug, P5010, P6050_3,
                                                       P6050_4, P6050_5, P6050_6, P6050_7, 
@@ -35,26 +33,6 @@ sub_set_training <- subset (training, select = -c(id, Li, Lp, ing, Npersug, P501
                                                       P6210s1_12, P6210s1_13, P6210s1_14, P6210s1_15,
                                                       P6210s1_2, P6210s1_4, P6210s1_5, P6210s1_6, P6210s1_7,
                                                       P6210s1_8, P6210s1_9, P6210s1_99, P7505_1))
-
-#"colnames(sub_set_training)
-
-#skim(sub_set_training)
-
-#library(fastDummies)
-
-#sub_set_training <- dummy_cols(sub_set_training,
-#                              select_columns = c("Clase", "P6090", "P6100", "P6210", 
-#                                                  "P6240", "P7505", "Depto", "P5090"))
-
-
-#sub_set_training <- subset(sub_set_training, select = -c(Clase, P6090, P6100, P6210, 
-#                                                        P6240, P7505, Depto, P5090))
-
-#sub_set_training <- subset(sub_set_training, select = -c(Clase_1, P6090_1, P6100_1, P6210_3, 
-#                                                         P6210_6, P6240_1, P6240_6, P7505_1, 
-#                                                         Depto_05, Depto_76, P5090_1, P5090_3))
-
-#colnames(sub_set_training)"
 
 
 ## Modelo
@@ -65,8 +43,8 @@ logit_training <- glm(Pobre_1 ~.,
 summary(logit_training)
 
 sub_set_training$Pobre_1_logit_training <- predict(logit_training,
-                                                 newdata = sub_set_training,
-                                                 type = "response") # Estimación de predicciones de Pobre_1za training
+                                                   newdata = sub_set_training,
+                                                   type = "response") # Estimación de predicciones de Pobre_1za training
 
 summary(sub_set_training$Pobre_1_logit_training)
 
@@ -357,16 +335,16 @@ train_oversampling <- subset(train_oversampling, select = -c(Pobre_1_logit_train
 
 colnames(train_oversampling)
 
-logit_oversampled <- glm(Pobre_1 ~.,
+logit_oversampling <- glm(Pobre_1 ~.,
                          data = train_oversampling, family = binomial(link="logit")) ## Estimar modelo logit en evaluate
 
-tidy(logit_oversampled)
-summary(logit_oversampled)
+tidy(logit_oversampling)
+summary(logit_oversampling)
 
-train_oversampling$Pobre_1_logit_oversampled <- predict(logit_oversampled,
-                                                        newdata = train_oversampling,
-                                                        type = "response") # Estimación de predicciones de Pobre_1za
-summary(train_oversampling$Pobre_1_logit_oversampled)
+train_oversampling$Pobre_1_logit_oversampling <- predict(logit_oversampling,
+                                                         newdata = train_oversampling,
+                                                         type = "response") # Estimación de predicciones de Pobre_1za
+summary(train_oversampling$Pobre_1_logit_oversampling)
 
 ggplot(data=train_oversampling , mapping=aes(Pobre_1,Pobre_1_logit_oversampled)) + 
   geom_boxplot(aes(fill=as.factor(Pobre_1))) + theme_test() ## la regra de bayes parece funcionar para predecir pobreza
@@ -428,6 +406,71 @@ abline(a = 0, b = 1)
 
 auc_ROC_r2 <- performance(predicciones_r2, measure = "auc")
 auc_ROC_r2@y.values[[1]]
+
+
+
+### Comparación con muestra evaluate
+
+
+Pobre_1_logit_oversampling_test <- predict(logit_oversampling,
+                                           newdata = sub_set_evaluating,
+                                           type = "response") # Estimación de predicciones de Pobre_1za
+
+colnames(sub_set_evaluating)
+
+ggplot(data=sub_set_evaluating , mapping=aes(Pobre_1,Pobre_1_logit_evaluating)) + 
+  geom_boxplot(aes(fill=as.factor(Pobre_1))) + theme_test() ## parece que 0.35 es el valor para evitar subestimar a los Pobre_1s
+
+
+regla1 <- 0.5 # Se define regla de Bayes
+
+sub_set_evaluating$Pobre_1_hat1_evaluating <- ifelse(sub_set_evaluating$Pobre_1_logit_evaluating>regla1,1,0) ## Prediccion de Pobre_1za
+
+
+ggplot(sub_set_evaluating, aes(x = Pobre_1)) +
+  geom_bar(fill = "darkblue") +
+  theme_bw() +
+  labs(title = "¿Es la persona Pobre_1?",
+       x = "",
+       y = "Distribución")
+
+ggplot(sub_set_evaluating, aes(x = Pobre_1_hat1_evaluating)) +
+  geom_bar(fill = "darkblue") +
+  theme_bw() +
+  labs(title = "¿Es la persona Pobre_1? - Predicción",
+       x = "",
+       y = "Distribución")
+
+cm_logit_evaluating_r1 <- confusionMatrix(data = factor(sub_set_evaluating$Pobre_1_hat1_evaluating),
+                                          reference = factor(sub_set_evaluating$Pobre_1),
+                                          mode = "sens_spec", positive = "1")
+
+cm_logit_evaluating_r1 <- cm_logit_evaluating_r1$table
+
+skim(sub_set_evaluating$Pobre_1)
+
+
+acc_evaluating_r1 <- Accuracy(y_pred = sub_set_evaluating$Pobre_1_hat1_evaluating, y_true = sub_set_evaluating$Pobre_1)
+pre_evaluating_r1 <- Precision(y_pred = sub_set_evaluating$Pobre_1_hat1_evaluating, y_true = sub_set_evaluating$Pobre_1, positive = 1)
+rec_evaluating_r1 <- Recall(y_pred = sub_set_evaluating$Pobre_1_hat1_evaluating, y_true = sub_set_evaluating$Pobre_1, positive = 1)
+f1_evaluating_r1 <- F1_Score(y_pred = sub_set_evaluating$Pobre_1_hat1_evaluating, y_true = sub_set_evaluating$Pobre_1, positive = 1)
+spf_evaluating_r1 <- Specificity(y_pred = sub_set_evaluating$Pobre_1_hat1_evaluating, y_true = sub_set_evaluating$Pobre_1, positive = 1)
+FPR_evaluating_r1 <- cm_logit_evaluating_r1[2,1]/sum(cm_logit_evaluating_r1[2,])
+FNR_evaluating_r1 <- cm_logit_evaluating_r1[1,2]/sum(cm_logit_evaluating_r1[1,])
+
+metricas_evaluating_r1 <- data.frame(Modelo = "Logit - Regla de Bayes",
+                                     "Muestreo" = NA, 
+                                     "Evaluación" = "Fuera de muestra",
+                                     "Accuracy" = acc_evaluating_r1,
+                                     "Precision" = pre_evaluating_r1,
+                                     "Recall" = rec_evaluating_r1,
+                                     "F1" = f1_evaluating_r1,
+                                     "Specificity" = spf_evaluating_r1,
+                                     "FPR" = FPR_evaluating_r1,
+                                     "FNR" = FNR_evaluating_r1)
+
+
+
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
